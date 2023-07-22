059739a386a60b3047efbc7649cb518df83130af
SametSever <139774058+SametSever@users.noreply.github.com>
2023-07-22 14:43:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def fact(sayi):
+    if sayi <= 1:
+        return 1
+    else:
+        return sayi * fact(sayi - 1)
+
+
+if __name__ == "__main__":
+    sayi = int(input("Faktoriyelini almak istediğiniz sayıyı giriniz : "))
+
+    print(fact(sayi))
